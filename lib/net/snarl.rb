d7d29052f6b211d4253f394b614fe805e2809ed4
Luis Lavena <luislavena@gmail.com>
2010-07-24 02:05:07 -0300

    Host and port.

--- lib/net/snarl.rb
+++ lib/net/snarl.rb
@@ -1,4 +1,10 @@
 module Net
   class Snarl
+    attr_reader :host, :port
+
+    def initialize(host = nil, port = nil)
+      @host = host || '127.0.0.1'
+      @port = (port || 9887).to_i
+    end
   end
 end
--- spec/net/snarl_spec.rb
+++ spec/net/snarl_spec.rb
@@ -1,4 +1,17 @@
 require 'net/snarl'
 
 describe Net::Snarl do
+  context 'when first created' do
+    it 'defaults connection to local server' do
+      snarl = Net::Snarl.new
+      snarl.host.should == '127.0.0.1'
+      snarl.port.should == 9887
+    end
+
+    it 'allows connection to remote server' do
+      snarl = Net::Snarl.new('remote', 1234)
+      snarl.host.should == 'remote'
+      snarl.port.should == 1234
+    end
+  end
 end
