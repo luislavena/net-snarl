b9576c187d823242699c870d2b8d15741ea66ed7
Luis Lavena <luislavena@gmail.com>
2010-07-25 17:02:29 -0300

    Added register, unregister, add_class and basic structure.

--- lib/net/snarl.rb
+++ lib/net/snarl.rb
@@ -5,6 +5,7 @@ module Net
     HEADER = "type=SNP".freeze
     PROTOCOL_VERSION = "version=1.0".freeze
     TERMINATOR = "\r\n".freeze
+    DEFAULT_TIMEOUT = 10
 
     attr_reader :host, :port
 
@@ -14,24 +15,50 @@ module Net
     end
 
     def notify(*args)
-      options = args.last || {}
-      options[:action] = 'notification'
+      params = args.last || {}
+      add_defaults(params)
 
-      TCPSocket.open(@host, @port) do |s|
-        s.write build_command(options)
-      end
+      response = connected { |c| c.write command('notification', params) }
+    end
+
+    def register(app)
+      response = connected { |c| c.write command('register', :app => app) }
+    end
+
+    def unregister(app)
+      response = connected { |c| c.write command('unregister', :app => app) }
+    end
+
+    def add_class(app, klass, *args)
+      params = args.last || {}
+      params[:app] = app
+      params[:class] = klass
+
+      response = connected { |c| c.write command('add_class', params) }
     end
 
     private
 
-    def build_command(params)
-      cmd = []
+    def command(action, params)
+      cmd = [HEADER, PROTOCOL_VERSION, "action=#{action}"]
+
       params.each do |k, v|
         cmd << "#{k}=#{v}"
       end
-      output = "type=SNP#?version=1.0"
-      output << "#?#{cmd.join('#?')}" unless cmd.empty?
-      output
+
+      cmd.join('#?') + TERMINATOR
+    end
+
+    def add_defaults(params)
+      params[:timeout] ||= DEFAULT_TIMEOUT
+      params
+    end
+
+    def connected
+      TCPSocket.open(@host, @port) do |s|
+        yield s
+        s.gets
+      end
     end
   end
 end
